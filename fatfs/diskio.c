2ce6b54a501f64db4c36f4baaeb5f6071f7f026c
Fedor <lfa1@yandex.ru>
2022-03-18 18:42:06 +0300

    Update diskio.c

--- fatfs/diskio.c
+++ fatfs/diskio.c
@@ -114,7 +114,7 @@ DRESULT ICACHE_FLASH_ATTR disk_ioctl (
 //            res = RES_OK;
 //            break;
 //        case GET_SECTOR_COUNT:
-//            *(DWORD*)buff = (sd_get_capacity()/sd_get_sector_size());
+//            *(DWORD*)buff = (uint32_t)(sd_get_capacity()/sd_get_sector_size());
 //            res = RES_OK;
 //            break;
 //        case GET_SECTOR_SIZE:
@@ -135,6 +135,7 @@ DRESULT ICACHE_FLASH_ATTR disk_ioctl (
 //    return res;
 //
 //    return RES_PARERR;
+
     if(cmd == CTRL_SYNC) {
         return RES_OK;
     } else {
